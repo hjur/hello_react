4d7d9c688029de9936b1859f019c7eba85dfd2c6
Hwang juri <hjurzz@gmail.com>
2020-01-03 18:56:29 +0900

    array add

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React, { Component, Fragment } from 'react';
 import MyName from './MyName';
 import Counter from './Counter';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component{
+  id = 2
   state={
-    info:''
+    info:'',
+    information: [
+      {
+        id:0,
+        name:'jju',
+        phone:'000-0000-0000'
+      },
+      {
+        id:1,
+        name:'jae',
+        phone:'111-1111-1111'
+      }
+    ]
   }
   handleCreate = (data) => {
+    const {information} = this.state;
     this.setState({
-      info:`${data.name} and phone number is ${data.phone}`
+      info:`${data.name[0]} and phone number is ${data.phone[0]}`,
+      information: information.concat({id:this.id++, ...data})
     })
   }
   render() {
+    const {information} = this.state;
     return(
       <Fragment>
         <MyName name={this.state.info}></MyName>
@@ -20,6 +37,8 @@ class App extends Component{
         <Counter></Counter>
         <div>============</div>
         <PhoneForm onCreate={this.handleCreate}></PhoneForm>
+        {JSON.stringify(information)}
+        <PhoneInfoList data={this.state.information}></PhoneInfoList>
       </Fragment>
     )
   }
