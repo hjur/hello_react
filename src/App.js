4fe1948d030d7273684f2f25c7a8e50564b25474
Hwang juri <hjurzz@gmail.com>
2020-01-03 17:15:11 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import MyName from './MyName';
+// import MyName from './MyName';
+import Counter from './Counter';
 
 class App extends Component{
   render() {
     return(
-      <MyName name="jju"></MyName>
+      // <MyName name="jju"></MyName>
       // <MyName></MyName>
+      <Counter></Counter>
     )
   }
 }
